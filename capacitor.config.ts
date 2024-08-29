17379e03a4a9cb6887202cd685f0e3487b8c1b41
CETECOM <cetecom_conce@duoc.cl>
2024-08-29 10:17:32 -0400

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ejemplo_app_s2',
+  webDir: 'www'
+};
+
+export default config;
